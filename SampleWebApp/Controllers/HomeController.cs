75c1a31517ea5606dfd6a857ed1b176486481918
Atul Verma <atverma@microsoft.com>
2019-05-19 20:23:15 -0400

    Sample ASP.NET Core 3.0 app with DockerFile (windows/iis)

---
+++ SampleWebApp/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SampleWebApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            return "Hello World : AKS - ASP.NET Core 3.0 Web API hosted in IIS and running in Windows Server 2019 Container";
+        }
+    }
+}
